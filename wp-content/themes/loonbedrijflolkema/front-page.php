b330c89752d5dfc097a6ddd7dca1e3a6c2ec0e97
Camilla Binni <milla.binni@gmail.com>
2023-09-04 22:20:06 -0300

    feat(*): Integração Home Page

--- wp-content/themes/loonbedrijflolkema/front-page.php
+++ wp-content/themes/loonbedrijflolkema/front-page.php
@@ -1,28 +1,24 @@
 <?php 
-/**
- * Template Name: Home / Front Page
- *
- **/
 	get_header();
 ?>
-
-	<div class="slider">
-		<div class="swiper slider-home">
-			<div class="swiper-wrapper">
-				<div class="swiper-slide">
-					<div class="slider-general" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/slider.png');"></div>
-				</div>
-				<div class="swiper-slide">
-					<div class="slider-general" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/Slider_2.jpg');"></div>
-				</div>
-				<div class="swiper-slide">
-					<div class="slider-general" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/image-1.jpg');"></div>
+	<?php if( have_rows('image_slider_home') ): ?>
+		<div class="slider">
+			<div class="swiper slider-home">
+				<div class="swiper-wrapper">
+					<?php
+						while( have_rows('image_slider_home') ) : the_row();
+						$image = get_sub_field('image');
+					?>
+						<div class="swiper-slide">
+							<div class="slider-general" style="background-image: url('<?php echo $image ?>');"></div>
+						</div>
+					<?php endwhile; ?>
 				</div>
 			</div>
-		</div>
 
-		<div class="swiper-pagination swiper-pagination-slider-home"></div>
-	</div>
+			<div class="swiper-pagination swiper-pagination-slider-home"></div>
+		</div>
+	<?php endif; ?>
 
 	<div class="section">
 		<div class="section-about">
@@ -30,11 +26,11 @@
 				<div class="row">
 					<div class="col-md-6 left">
 						<div class="text-box">
-							<p>Heb je een vraag of wil je gebruik maken van één van onze diensten?</p>
+							<p><?php echo esc_html( get_field('text_above_the_button') ); ?></p>
 
 							<div class="button mob-mb-30">
-								<button class="button-default">
-									Lorem Ipsum
+								<a href="<?php echo esc_html( get_field('button_link_about') ); ?>" class="button-default">
+									<?php echo esc_html( get_field('button_text_about') ); ?>
 
 									<div class="hexagon">
 										<div class="hexagon-inner">
@@ -45,167 +41,109 @@
 											</div>
 										</div>
 									</div>
-								</button>
+								</a>
 							</div>
 						</div>
 					</div>
 
 					<div class="col-md-6 right">
-						<p>In meer dan 40 jaar tijd is Loonbedrijf Lolkema uitgegroeid van een eenmanszaak tot een onderneming met gecertificeerde medewerkers. Met een actueel machinepark wordt een grote verscheidenheid aan werkzaamheden uitgevoerd in de agrarische sector en de grond, weg en waterbouw. Door een flexibele inzet van een gemotiveerd team wordt elke dag gestreefd naar een hoge klanttevredenheid.</p>
+						<p><?php echo esc_html( get_field('text_on_the_right') ); ?></p>
 					</div>
 				</div>
 			</div>
 		</div>
 
-		<div class="section-services">
-			<div class="container">
-				<div class="section-services-top">
-					<h1 class="title white">Onze Services</h1>
+		<?php if( have_rows('services') ): ?>
+			<div class="section-services">
+				<div class="container">
+					<div class="section-services-top">
+						<h1 class="title white">Onze Services</h1>
 
-					<div class="swiper-navigation-slider">
-						<div class="hexagon swiper-button-prev">
-							<div class="hexagon-inner">
-								<div class="hexagon-inner-in primary">
-									<svg xmlns="http://www.w3.org/2000/svg" width="21" height="22" viewBox="0 0 21 22" fill="none">
-										<path d="M6.57227 9.86974L11.8848 4.28483C12.252 3.89882 12.8457 3.89882 13.209 4.28483L14.0918 5.21291C14.459 5.59893 14.459 6.22312 14.0918 6.60503L10.3262 10.5637L14.0918 14.5225C14.459 14.9085 14.459 15.5327 14.0918 15.9146L13.2129 16.8509C12.8457 17.2369 12.252 17.2369 11.8887 16.8509L6.57617 11.266C6.20508 10.8799 6.20508 10.2558 6.57227 9.86974Z" fill="white"/>
-									</svg>
-								</div>
-							</div>
-						</div>
-						<div class="hexagon swiper-button-next">
-							<div class="hexagon-inner">
-								<div class="hexagon-inner-in primary">
-									<svg xmlns="http://www.w3.org/2000/svg" width="21" height="22" viewBox="0 0 21 22" fill="none">
-										<path d="M14.4277 12.0231L9.11523 17.608C8.74805 17.994 8.1543 17.994 7.79102 17.608L6.9082 16.6799C6.54102 16.2939 6.54102 15.6697 6.9082 15.2878L10.6738 11.3291L6.9082 7.37037C6.54102 6.98435 6.54102 6.36016 6.9082 5.97825L7.78711 5.04195C8.1543 4.65594 8.74805 4.65594 9.11133 5.04195L14.4238 10.6269C14.7949 11.0129 14.7949 11.6371 14.4277 12.0231Z" fill="white"/>
-									</svg>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="box-hexagon">
-					<div class="slider swiper-services">
-						<div class="swiper-wrapper">
-							<div class="swiper-slide">
-							<div class="poligono-slider" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/image-1.jpg');">
-								<div class="box-hexagon-text">
-									<p>Agrarisch Loonwerk</p>
-
-									<div class="hexagon">
-										<div class="hexagon-inner">
-											<div class="hexagon-inner-in">
-												<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-													<path d="M12.2831 9.41547L7.89448 13.8114C7.59115 14.1153 7.10065 14.1153 6.80054 13.8114L6.07125 13.0809C5.76792 12.7771 5.76792 12.2858 6.07125 11.9852L9.18204 8.8692L6.07125 5.75324C5.76792 5.4494 5.76792 4.95808 6.07125 4.65748L6.79732 3.92051C7.10065 3.61667 7.59115 3.61667 7.89126 3.92051L12.2799 8.31647C12.5865 8.62031 12.5865 9.11163 12.2831 9.41547Z" fill="black"/>
-												</svg>
-											</div>
-										</div>
+						<div class="swiper-navigation-slider">
+							<div class="hexagon swiper-button-prev">
+								<div class="hexagon-inner">
+									<div class="hexagon-inner-in primary">
+										<svg xmlns="http://www.w3.org/2000/svg" width="21" height="22" viewBox="0 0 21 22" fill="none">
+											<path d="M6.57227 9.86974L11.8848 4.28483C12.252 3.89882 12.8457 3.89882 13.209 4.28483L14.0918 5.21291C14.459 5.59893 14.459 6.22312 14.0918 6.60503L10.3262 10.5637L14.0918 14.5225C14.459 14.9085 14.459 15.5327 14.0918 15.9146L13.2129 16.8509C12.8457 17.2369 12.252 17.2369 11.8887 16.8509L6.57617 11.266C6.20508 10.8799 6.20508 10.2558 6.57227 9.86974Z" fill="white"/>
+										</svg>
 									</div>
 								</div>
 							</div>
-							</div>
-							<div class="swiper-slide">
-							<div class="poligono-slider" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/image-2.jpg');">
-								<div class="box-hexagon-text">
-									<p>Grondverzet & Infra</p>
-									
-									<div class="hexagon">
-										<div class="hexagon-inner">
-											<div class="hexagon-inner-in">
-												<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-													<path d="M12.2831 9.41547L7.89448 13.8114C7.59115 14.1153 7.10065 14.1153 6.80054 13.8114L6.07125 13.0809C5.76792 12.7771 5.76792 12.2858 6.07125 11.9852L9.18204 8.8692L6.07125 5.75324C5.76792 5.4494 5.76792 4.95808 6.07125 4.65748L6.79732 3.92051C7.10065 3.61667 7.59115 3.61667 7.89126 3.92051L12.2799 8.31647C12.5865 8.62031 12.5865 9.11163 12.2831 9.41547Z" fill="black"/>
-												</svg>
-											</div>
-										</div>
+							<div class="hexagon swiper-button-next">
+								<div class="hexagon-inner">
+									<div class="hexagon-inner-in primary">
+										<svg xmlns="http://www.w3.org/2000/svg" width="21" height="22" viewBox="0 0 21 22" fill="none">
+											<path d="M14.4277 12.0231L9.11523 17.608C8.74805 17.994 8.1543 17.994 7.79102 17.608L6.9082 16.6799C6.54102 16.2939 6.54102 15.6697 6.9082 15.2878L10.6738 11.3291L6.9082 7.37037C6.54102 6.98435 6.54102 6.36016 6.9082 5.97825L7.78711 5.04195C8.1543 4.65594 8.74805 4.65594 9.11133 5.04195L14.4238 10.6269C14.7949 11.0129 14.7949 11.6371 14.4277 12.0231Z" fill="white"/>
+										</svg>
 									</div>
 								</div>
-							</div>
-							</div>
-							<div class="swiper-slide">
-							<div class="poligono-slider" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/image-3.jpg');">
-								<div class="box-hexagon-text">
-									<p>Cultuurtechnisch werk</p>
-									
-									<div class="hexagon">
-										<div class="hexagon-inner">
-											<div class="hexagon-inner-in">
-												<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-													<path d="M12.2831 9.41547L7.89448 13.8114C7.59115 14.1153 7.10065 14.1153 6.80054 13.8114L6.07125 13.0809C5.76792 12.7771 5.76792 12.2858 6.07125 11.9852L9.18204 8.8692L6.07125 5.75324C5.76792 5.4494 5.76792 4.95808 6.07125 4.65748L6.79732 3.92051C7.10065 3.61667 7.59115 3.61667 7.89126 3.92051L12.2799 8.31647C12.5865 8.62031 12.5865 9.11163 12.2831 9.41547Z" fill="black"/>
-												</svg>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							</div>
-							<div class="swiper-slide">
-							<div class="poligono-slider" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/image-4.jpg');">
-								<div class="box-hexagon-text">
-									<p>Civiele Techniek</p>
-									
-									<div class="hexagon">
-										<div class="hexagon-inner">
-											<div class="hexagon-inner-in">
-												<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-													<path d="M12.2831 9.41547L7.89448 13.8114C7.59115 14.1153 7.10065 14.1153 6.80054 13.8114L6.07125 13.0809C5.76792 12.7771 5.76792 12.2858 6.07125 11.9852L9.18204 8.8692L6.07125 5.75324C5.76792 5.4494 5.76792 4.95808 6.07125 4.65748L6.79732 3.92051C7.10065 3.61667 7.59115 3.61667 7.89126 3.92051L12.2799 8.31647C12.5865 8.62031 12.5865 9.11163 12.2831 9.41547Z" fill="black"/>
-												</svg>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							</div>
-							<div class="swiper-slide">
-							<div class="poligono-slider" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/image-5.jpg');">
-								<div class="box-hexagon-text">
-									<p>Verhuur</p>
-									
-									<div class="hexagon">
-										<div class="hexagon-inner">
-											<div class="hexagon-inner-in">
-												<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-													<path d="M12.2831 9.41547L7.89448 13.8114C7.59115 14.1153 7.10065 14.1153 6.80054 13.8114L6.07125 13.0809C5.76792 12.7771 5.76792 12.2858 6.07125 11.9852L9.18204 8.8692L6.07125 5.75324C5.76792 5.4494 5.76792 4.95808 6.07125 4.65748L6.79732 3.92051C7.10065 3.61667 7.59115 3.61667 7.89126 3.92051L12.2799 8.31647C12.5865 8.62031 12.5865 9.11163 12.2831 9.41547Z" fill="black"/>
-												</svg>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
 							</div>
 						</div>
 					</div>
 
-					<div class="swiper-pagination swiper-pagination-slider-services"></div>
-				</div>
+					<div class="box-hexagon">
+						<div class="slider swiper-services">
+							<div class="swiper-wrapper">
+								<?php
+									while( have_rows('services') ) : the_row();
 
-				<div class="button">
-					<button class="button-default">
-						Lorem Ipsum
+									$image = get_sub_field('image');
+									$title = get_sub_field('title');
+									$link = get_sub_field('link');
+								?>
+									<div class="swiper-slide">
+										<div class="poligono-slider" style="background-image: url('<?php echo $image ?>');">
+											<a href="<?php echo $link ?>" class="box-hexagon-text">
+												<p><?php echo $title ?></p>
 
-						<div class="hexagon">
-							<div class="hexagon-inner">
-								<div class="hexagon-inner-in">
-									<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-										<path d="M12.2831 9.41547L7.89448 13.8114C7.59115 14.1153 7.10065 14.1153 6.80054 13.8114L6.07125 13.0809C5.76792 12.7771 5.76792 12.2858 6.07125 11.9852L9.18204 8.8692L6.07125 5.75324C5.76792 5.4494 5.76792 4.95808 6.07125 4.65748L6.79732 3.92051C7.10065 3.61667 7.59115 3.61667 7.89126 3.92051L12.2799 8.31647C12.5865 8.62031 12.5865 9.11163 12.2831 9.41547Z" fill="black"/>
-									</svg>
-								</div>
+												<div class="hexagon">
+													<div class="hexagon-inner">
+														<div class="hexagon-inner-in">
+															<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+																<path d="M12.2831 9.41547L7.89448 13.8114C7.59115 14.1153 7.10065 14.1153 6.80054 13.8114L6.07125 13.0809C5.76792 12.7771 5.76792 12.2858 6.07125 11.9852L9.18204 8.8692L6.07125 5.75324C5.76792 5.4494 5.76792 4.95808 6.07125 4.65748L6.79732 3.92051C7.10065 3.61667 7.59115 3.61667 7.89126 3.92051L12.2799 8.31647C12.5865 8.62031 12.5865 9.11163 12.2831 9.41547Z" fill="black"/>
+															</svg>
+														</div>
+													</div>
+												</div>
+											</a>
+										</div>
+									</div>
+								<?php endwhile; ?>
 							</div>
 						</div>
-					</button>
+
+						<div class="swiper-pagination swiper-pagination-slider-services"></div>
+					</div>
+
+					<div class="button">
+						<a href="<?php echo esc_html( get_field('button_link_service') ); ?>" class="button-default">
+							<?php echo esc_html( get_field('button_text_service') ); ?>
+
+							<div class="hexagon">
+								<div class="hexagon-inner">
+									<div class="hexagon-inner-in">
+										<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+											<path d="M12.2831 9.41547L7.89448 13.8114C7.59115 14.1153 7.10065 14.1153 6.80054 13.8114L6.07125 13.0809C5.76792 12.7771 5.76792 12.2858 6.07125 11.9852L9.18204 8.8692L6.07125 5.75324C5.76792 5.4494 5.76792 4.95808 6.07125 4.65748L6.79732 3.92051C7.10065 3.61667 7.59115 3.61667 7.89126 3.92051L12.2799 8.31647C12.5865 8.62031 12.5865 9.11163 12.2831 9.41547Z" fill="black"/>
+										</svg>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
 				</div>
 			</div>
-		</div>
+		<?php endif; ?>
 
 		<div class="section-about">
 			<div class="container">
 				<div class="row">
 					<div class="col-md-12 left">
 						<div class="text-box">
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit morbi laoreet neque</p>
+							<p><?php echo esc_html( get_field('text') ); ?></p>
 
 							<div class="button">
-								<button class="button-default">
-									Lorem Ipsum
+								<a href="<?php echo esc_html( get_field('link_button_contact') ); ?>" class="button-default">
+									<?php echo esc_html( get_field('button_text_contact') ); ?>
 
 									<div class="hexagon">
 										<div class="hexagon-inner">
@@ -216,7 +154,7 @@
 											</div>
 										</div>
 									</div>
-								</button>
+								</a>
 							</div>
 						</div>
 					</div>
